0a27f2a3f9cdd9183d9ff28f967a5c8912ccc026
mszkb <mszkb@skca.eu>
2019-07-09 12:39:26 +0200

    move RepRdfUtils and REP vocab into cmd tool

---
+++ src/main/java/msz/bakk/protocol/Utils/WonRepRdfUtils.java
@@ -0,0 +1,31 @@
+package msz.bakk.protocol.Utils;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import won.protocol.util.WonRdfUtils;
+
+public class WonRepRdfUtils extends WonRdfUtils {
+    private static final Logger logger = LoggerFactory.getLogger(WonRepRdfUtils.class);
+
+    public static Model addStuff(Model model) {
+        // Model messageModel = createModelWithBaseResource();
+        // Resource baseRes =
+        // messageModel.createResource(messageModel.getNsPrefixURI(""));
+        // baseRes.addProperty(WONCON.text, message, XSDDatatype.XSDstring);
+        // return messageModel;
+        return null;
+    }
+
+    public static Model createBaseModel() {
+        return createModelWithBaseResource();
+    }
+
+    private static Model createModelWithBaseResource() {
+        Model model = ModelFactory.createDefaultModel();
+        model.setNsPrefix("", "no:uri");
+        model.createResource(model.getNsPrefixURI(""));
+        return model;
+    }
+}
